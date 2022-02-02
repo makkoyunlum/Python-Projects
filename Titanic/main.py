9558a4101190a88147735953c0b2ccc020e94c42
makkoyunlum <98486029+makkoyunlum@users.noreply.github.com>
2022-02-02 03:00:01 +0300

    Created Titanic Project

---
+++ Titanic/main.py
@@ -0,0 +1,45 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-whitegrid")
+
+import seaborn as sns
+
+from collections import Counter
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+
+# dir(plt)
+# print(plt.style.available)
+
+
+train_df = pd.read_csv("C:\\Users\\AK\\Desktop\\Python-Project-Git\\Python-Projects\\Titanic\\datas\\train.csv")
+test_df = pd.read_csv("C:\\Users\\AK\\Desktop\\Python-Project-Git\\Python-Projects\\Titanic\\datas\\test.csv")
+test_PassengerId = test_df["PassengerId"]
+
+train_df.columns
+
+train_df.head()
+
+train_df.describe()
+
+print(train_df.head())
